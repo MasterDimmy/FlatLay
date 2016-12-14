28a35b30678c0c82e698d1af984a4a6e8ffc12a0
dk <you@example.com>
2016-12-14 22:52:40 +0400

    db ok

--- create_db/main.go
+++ create_db/main.go
@@ -42,13 +42,17 @@ type TImage struct {
 	Width  int    //ширина
 	Height int    //высота
 	Weight int    //архимедов вес
+	Group  int    //группа стиля одежды (категория?)
 }
 
 type TDatabase struct {
 	Images []TImage
 }
 
-var db TDatabase
+var db TDatabase //вся база
+
+var names = make(map[string]int) //номер  группы (категория картинки)
+var min_name_len = 10000
 
 func walkFn(path string, info os.FileInfo, err error) error {
 	if info == nil {
@@ -77,6 +81,11 @@ func walkFn(path string, info os.FileInfo, err error) error {
 		Width:  img.Bounds().Max.X,
 		Height: img.Bounds().Max.Y,
 		Weight: 1,
+		Group:  0, //значение по умолчанию
+	}
+
+	if min_name_len > len(image.Name) {
+		min_name_len = len((image.Name))
 	}
 
 	db.Images = append(db.Images, image)
@@ -111,24 +120,69 @@ func main() {
 		return
 	}
 
+	//обход каталога - построение модели базы в памяти
 	err = filepath.Walk(os.Args[1], walkFn)
 	if err != nil {
 		fmt.Println("Ошибка: " + err.Error())
 		return
 	}
 
+	//формируем группы одежды в рамках одного стиля на основе имени файла
+	/* из имен файлов
+	23.11.2016  22:29            90 091 produktybermudy-kargo-loose-fit-sinij-v-kletku-923883stl.jpg
+	23.11.2016  22:29            16 534 produktybermudy-kargo-loose-fit-sinij-v-kletku-923883stl_product1.jpg
+	23.11.2016  22:29             5 292 produktybermudy-kargo-loose-fit-sinij-v-kletku-923883stl_product2.jpg
+	23.11.2016  22:29            35 287 produktybermudy-kargo-loose-fit-sinij-v-kletku-923883stl_product3.jpg
+	23.11.2016  22:29             6 272 produktybermudy-kargo-loose-fit-sinij-v-kletku-923883stl_product4.jpg
+	23.11.2016  22:28            81 629 produktybrjuki-5-karmanov-regular-fit-straight-bezhevyj-907597stl.jpg
+	23.11.2016  22:28            22 252 produktybrjuki-5-karmanov-regular-fit-straight-bezhevyj-907597stl_product1.jpg
+	23.11.2016  22:28             5 830 produktybrjuki-5-karmanov-regular-fit-straight-bezhevyj-907597stl_product2.jpg
+	23.11.2016  22:28            18 011 produktybrjuki-5-karmanov-regular-fit-straight-bezhevyj-907597stl_product3.jpg
+	23.11.2016  22:28            16 156 produktybrjuki-5-karmanov-regular-fit-straight-bezhevyj-907597stl_product4.jpg
+	23.11.2016  22:30           109 119 produktybrjuki-bez-zastezhki-classic-fit-sinij-939493stl.jpg
+	23.11.2016  22:30            20 835 produktybrjuki-bez-zastezhki-classic-fit-sinij-939493stl_product1.jpg
+	23.11.2016  22:30            19 700 produktybrjuki-bez-zastezhki-classic-fit-sinij-939493stl_product2.jpg
+	23.11.2016  22:30             4 737 produktybrjuki-bez-zastezhki-classic-fit-sinij-939493stl_product3.jpg
+
+	выделяем самое короткое имя produktybrjuki-bez-zastezhki-classic-fit-sinij-939493stl.jpg
+	убираем все до .
+	считаем эту длину "базовой"
+	все имена, что входят в "базу" - принадлежат одной группе
+
+	produktybrjuki-bez-zastezhki-classic-fit-sinij-939493stl
+	produktybrjuki-bez-zastezhki-classic-fit-sinij-939493stl_product1
+	produktybrjuki-bez-zastezhki-classic-fit-sinij-939493stl_product2
+	produktybrjuki-bez-zastezhki-classic-fit-sinij-939493stl_product3
+	*/
+
+	//формируем базу имен
+	for _, i := range db.Images {
+		base := i.Name[:min_name_len]
+		_, ok := names[base]
+		if !ok {
+			names[base] = len(names) + 1
+		}
+	}
+
+	//формируем группы картинок на основе базы имен
+	for n, i := range db.Images {
+		db.Images[n].Group = names[i.Name[:min_name_len]]
+	}
+
+	//формируем модель базы json
 	data, err := json.MarshalIndent(db, "", " ")
 	if err != nil {
 		fmt.Println("Ошибка: " + err.Error())
 		return
 	}
 
+	//пишем
 	err = ioutil.WriteFile("database.json", data, 0777)
 	if err != nil {
 		fmt.Println("Ошибка: " + err.Error())
 		return
 	}
 
-	fmt.Printf("База database.json создана!\nЗаписей: %d", len(db.Images))
+	fmt.Printf("База database.json создана!\nЗаписей: %d\nКатегорий: %d", len(db.Images), len(names))
 	return
 }
